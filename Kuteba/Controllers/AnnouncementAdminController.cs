56f950c56765f97a87beca48b8e952943fe2d272
Donayam <donayam.nega@yahoo.com>
2018-08-15 11:53:35 +0300

    Added Anouncement Admin Layout

---
+++ Kuteba/Controllers/AnnouncementAdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kuteba.Controllers
+{
+    public class AnnouncementAdminController : Controller
+    {
+        // GET: AnnouncementAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List() {
+            return View();
+        }
+        public ActionResult Edit() {
+            return View();
+        }
+
+    }
+}
