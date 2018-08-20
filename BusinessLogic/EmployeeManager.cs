a34241de6b85460f17e92fe291371f86aeffc067
yonatan <yonatane504@gmail.com>
2018-08-20 20:20:51 +0300

    Created Rudementarry Employee manager consisting of Controller and view Model.

---
+++ BusinessLogic/EmployeeManager.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using ViewModels;
+
+namespace BusinessLogic
+{
+    public class EmployeeManager : IEmployeeManager
+    {
+        Employee emp;
+        public EmployeeManager(Employee emp)
+        {
+            this.emp = emp;
+        }
+
+        public Employee AddEmployee(EmployeeViewmodel evm)
+        {
+            emp.Name = evm.EmploeeName;
+            emp.EmployeeId = evm.EmployeeID;
+            return emp;
+        }
+    }
+}
---
+++ ViewModels/EmployeeViewmodel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels
+{
+    public class EmployeeViewmodel
+    {
+        [Required(ErrorMessage = "Employee Name is required.")]//handles or captures The Name Of the Employee
+        public string EmploeeName { get; set; }
+
+        [Required(ErrorMessage = "Employee ID is required.")]//Handles or Captures the Employees ID
+        public  String EmployeeID { get; set; }
+
+        //Code after this To Be Added After Edit of Domain
+
+        /*
+          [Required(ErrorMessage = "Employee Birthdate is required.")]
+          public string Birthday{ get; set; }
+
+          [Required(ErrorMessage = "Employee Picture is required.")]
+          public string  ID_Picture{ get; set; }
+
+         */
+
+    }
+}
