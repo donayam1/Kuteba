6c31a982c48d57e50e1a250fb34f75e710d1e7e4
sari <sari>
2018-08-22 11:04:02 +0300

    New Employee Form A new information about the employee is registered Set Salary form holds the financial records of the employee

---
+++ Kuteba/Controllers/AddEmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kuteba.Controllers
+{
+    public class AddEmployeeController : Controller
+    {
+        // GET: Add
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
