91a492007dc20949f6e2a97497846879fcb84441
Maya <mahlateyared@gmail.com>
2018-08-24 04:51:34 +0300

    Renamed employeeId Controller.

---
+++ Kuteba/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kuteba.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeId
+      
+        public ActionResult EmployeeId()
+        {
+            return View();
+        }
+    }
+}
