4eb4b0c639d9b5ccf068c18636ac69c245f57c4b
yonatan <yonatane504@gmail.com>
2018-08-24 12:14:43 +0300

    Re-Comit

--- BusinessLogic/SignupService.cs
+++ BusinessLogic/SignupService.cs
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
-using Database;
-using ViewModels;
-
-namespace BusinessLogic
-{
-    public class SignupService : ISignupService
-    {
-        KutebaDatabase kdb;
-
-        public SignupService(KutebaDatabase db)
-        {
-            //set Database
-            kdb = db;
-        }
-
-        public User Signup(UserViewModel_Signup u)
-        {
-            User usr = new User();
-            usr.UserName = u.Username;
-            usr.Password = u.password;
-            usr.isPendding = true;
-
-            kdb.Users.Add(usr);
-            return usr;
-        }
-    }
-}
-=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -82,4 +47,4 @@ namespace BusinessLogic
         }
     }
 }
->>>>>>> f924cfd60f1b468cf4ae590f7cf6b752061afbc3
+
