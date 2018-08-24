3b1d5e6f312d0f67018f9edff5afc4589251fab7
Donayam <donayam.nega@yahoo.com>
2018-08-24 10:12:48 +0300

    Updated Database. Fixed basic problems.

---
+++ Domain/SavingAmounts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SavingAmounts
+    {
+        [Key]
+        public String Id { get; set; }
+        public int Amount { get; set; }
+        public DateTime ChagedDate { get; set; }
+        public bool isActive { get; set; }
+        /// <summary>
+        /// In Percent, or Birr
+        /// </summary>
+        public String Uint { get; set; }
+
+    }
+}
