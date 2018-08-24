10d086e8b296ac8582f9140fd845e26a95e3cbea
Athena <athena@gitub.com>
2018-08-24 10:08:12 -0800

    ListUSser

---
+++ BusinessLogic/IListUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using ViewModels;
+using Database;
+
+namespace BusinessLogic
+{
+    public interface IListUserService
+    {
+        
+        List<User> ListUsers();
+    }
+}
