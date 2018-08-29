d2cac211e945ba58dcc2947913210931ba442bf2
Donayam <donayam.nega@yahoo.com>
2018-08-29 11:13:43 +0300

    Re update Employee.

--- Domain/Employee.cs
+++ Domain/Employee.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain
 {
@@ -11,8 +10,9 @@ namespace Domain
         public String EmployeeId { get; set; }
         public String Name { get; set; }
         public String ProfilePicture { get; set; }
-        [Column (TypeName = "datetime2")]
         public DateTime Birthday { get; set; }
+        public float InitialSavings { get; set; }
+
 
     }
 }
