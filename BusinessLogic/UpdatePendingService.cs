8c0389dc1c0abd75a0bd3b1231d374260db028e3
Natnael Tilaye <natnaeltilaye30@gmail.com>
2018-08-29 11:33:10 +0300

    Update Pending Services.

---
+++ BusinessLogic/UpdatePendingService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Database;
+
+namespace BusinessLogic
+{
+    public class UpdatePendingService : IUpdatePendingService
+    {
+        KutebaDatabase kdb;
+        public UpdatePendingService(KutebaDatabase kdb) {
+            this.kdb = kdb;
+        }
+
+        public User pendingController(string UserId, bool status) {
+            var usr = kdb.Users.Where(u1 => u1.UserName == UserId).FirstOrDefault();
+            if (usr != null && usr.isPendding)
+            {
+                usr.isPendding = !usr.isPendding;
+                return usr;
+            }
+            else
+                return null;
+        }
+    }
+}
--- Kuteba/Controllers/UserAdminController.cs
+++ Kuteba/Controllers/UserAdminController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Domain;
+using Database;
+using BusinessLogic;
 
 namespace Kuteba.Controllers
 {
     [Authorize]
     public class UserAdminController : Controller
     {
+        KutebaDatabase kdb;
+        IUpdatePendingService pendingServices;
+        public UserAdminController() {
+            pendingServices = new UpdatePendingService(kdb);
+        }
         [HttpPost]
         // GET: UserAdmin
-        public ActionResult UpdatePending(String UserId,Boolean status)
+        public ActionResult UpdatePending(string UserId,bool status)
         {
-            
+            pendingServices.pendingController(UserId, status);
             return View();
         }
     }
