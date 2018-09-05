d0b5cfc647246556a416eece710022e1945d8f57
Donayam <donayam.nega@yahoo.com>
2018-09-05 09:54:28 +0300

    Added Anouncement service

---
+++ BusinessLogic/IAnnouncementEditService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using ViewModels;
+namespace BusinessLogic
+{
+    public interface IAnnouncementService
+    {
+        Announcemnets EditNewAnnouncement(AnnouncementEditViewModel avm);
+    }
+}
--- Kuteba/Controllers/AnnouncementAdminController.cs
+++ Kuteba/Controllers/AnnouncementAdminController.cs
@@ -3,21 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Database;
+using BusinessLogic;
+using ViewModels;
+using Domain;
 
 namespace Kuteba.Controllers
 {
     public class AnnouncementAdminController : Controller
     {
+        IAnnouncementService iaes;
+        public AnnouncementAdminController()
+        {
+            KutebaDatabase kdb = new KutebaDatabase();
+            iaes = new AnnouncementService(kdb);
+        }
         // GET: AnnouncementAdmin
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult List() {
+        public ActionResult List()
+        {
             return View();
         }
-        public ActionResult Edit() {
+        [HttpGet]
+        public ActionResult Edit()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(AnnouncementEditViewModel avm)
+        {
+            if (ModelState.IsValid)
+            {
+                Announcemnets ans = iaes.EditNewAnnouncement(avm);
+                return Redirect(Url.Action("Edit", "Index"));
+            }
+            else
+            {
+                ModelState.AddModelError("", "Incomplete form. Please try again.");
+                return View();
+            }
         }
 
     }
