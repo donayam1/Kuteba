5d20a861b70f1d208f9a239354bcf63ccf4f1437
Donayam <donayam.nega@yahoo.com>
2018-09-14 10:01:07 +0300

    Added Employee Lone service and db

---
+++ Domain/EmployeeLoan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+   public class EmployeeLoan
+    {
+        
+        public double LoanAmount { get; set; }
+
+        public string EmployeeID { get; set; }
+
+        public string LoanDate { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public double interest { get; set; }
+
+    }
+}
---
+++ ViewModels/EmployeeLoanVModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ViewModels
+{
+   public class EmployeeLoanVModel
+    {
+        [Required(ErrorMessage ="Required")]
+        public double LoanAmount { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string EmployeeID { get; set; }
+
+        [Required(ErrorMessage ="Required")]
+        public DateTime LoanDate { get; set; }
+
+        [Required(ErrorMessage ="Required")]
+        public string PictureURL { get; set; }
+
+        [Required(ErrorMessage ="Required")]
+        public double interest { get; set; }
+    }
+}
